9590c6f1333512a42de003f55960898047786749
chauhanrakshit242-sys <chauhanrakshit242@gmail.com>
2026-06-16 10:42:21 +0530

    Time: 511 ms (74.83%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -2,6 +2,6 @@
 
 Select  P.project_id ,Round(IFNULL(sum(E.experience_years)/count(e.experience_years),0),2)as average_years
 from Project P
-Left join  Employee E
+join  Employee E
 on P.employee_id = E.employee_id
 group by P.project_id;
