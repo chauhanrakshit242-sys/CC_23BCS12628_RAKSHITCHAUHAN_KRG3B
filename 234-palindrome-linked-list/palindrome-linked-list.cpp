9bc19db08c2471e83df3dfebfc6449908bee06eb
chauhanrakshit242-sys <chauhanrakshit242@gmail.com>
2026-06-26 22:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 117.9 MB (81.63%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middle(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL)
+        {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return true;
+
+        ListNode* firstHalfEnd = middle(head);
+
+        ListNode* secondHalfStart = reverse(firstHalfEnd->next);
+
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfStart;
+
+        while(p2 != NULL)
+        {
+            if(p1->val != p2->val)
+                return false;
+
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+
+        return true;
+    }
+};
