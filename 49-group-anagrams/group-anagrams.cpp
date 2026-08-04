6f69db52821603d9d71e7038f6650c2516c3cd43
chauhanrakshit242-sys <chauhanrakshit242@gmail.com>
2026-08-04 23:42:15 +0530

    Time: 59 ms (14.43%) | Memory: 35.5 MB (6.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    string getcode(string s)
-    {
-        int n = s.length();
-        vector<int>count(26,0);
-        for(char ch : s)
-        {
-            count[ch-'a']++;
-        }
-        string res="";
+    string getCode(const string& s) {
+        vector<int> count(26, 0);
 
-for (int i = 0; i < 26; i++)
-{
-    if (count[i] > 0)
-    {
-        res.push_back('a' + i);
-        res += to_string(count[i]);
-        res.push_back('#');
+        for (char ch : s)
+            count[ch - 'a']++;
+
+        string key;
+        for (int i = 0; i < 26; i++) {
+            key += "#";
+            key += to_string(count[i]);
+        }
+
+        return key;
     }
-}
-        return res;
-    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        for(string str : strs)
-        {
-            string temp = getcode(str);
-            mp[temp].push_back(str);
+        unordered_map<string, vector<string>> mp;
+
+        for (const string& str : strs) {
+            string key = getCode(str);
+            mp[key].push_back(str);
         }
-        vector<vector<string>>ans;
-        for(auto it : mp)
-        {
+
+        vector<vector<string>> ans;
+
+        for (auto& it : mp)
             ans.push_back(it.second);
-        }
+
         return ans;
     }
 };
